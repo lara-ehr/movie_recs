9fa5bd4130f2c06bb229008428751b5430a16a4f
snay <feligges@gmx.de>
2020-09-21 18:04:15 +0200

    Lara's changes

--- create_movie_db.py
+++ create_movie_db.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+
 import psycopg2
 
 from credentials import *
@@ -12,7 +14,7 @@ ratings = pd.read_csv(PATH_RATINGS)
 ratings = ratings.drop('timestamp', axis = 1)
 ratings.columns = ['userid', 'movieid', 'ratings']
 
-conn = f'postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+CONN = f'postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
 drop_tables_query = '''
 DROP TABLE IF EXISTS movies;
@@ -26,41 +28,35 @@ movieid SERIAL PRIMARY KEY,
 title VARCHAR(250),
 genres VARCHAR(150)
 );
-
 CREATE TABLE links (
 movieid SERIAL PRIMARY KEY,
 imdbid VARCHAR(10),
 tmdbid BIGINT
 );
-
 '''
 
-def scale(df):
-    '''
-    Normalise on min-max scale by user
-    '''
-    groups = df.groupby('userid')
-    mean = groups.transform(np.min)
-    max = groups.transform(np.max)
-    norm = (df[min.columns] - min) / (max - min)
-    return norm['ratings']
+ratings['mean'] = ratings.groupby('userid').transform('mean')['ratings']
+ratings['demeaned'] = ratings['ratings'] - ratings['mean']
+ratings = ratings.drop(['mean'], axis=1)
 
-ratings['norm'] = scale(ratings, 'userid')
-
-ratings['de_meaned'] = ratings.groupby('userid').transform('mean')['ratings']
 
 def copy_table(table_name, path):
-    query = f"COPY {table_name} FROM \'{path}\' DELIMITER ',' CSV HEADER;"
+    '''
+    Input: name of table as string, path as variable (defined in separate credentials.py script)
+    Output: query string
+    '''
+    query = f"COPY {table_name} FROM \'{path}\' DELIMITER ',' CSV HEADER; COMMIT;"
     return query
+
 
 if __name__ == '__main__':
 
-    engine = create_engine(conn, encoding = 'latin1', echo= False)
+    engine = create_engine(CONN, encoding = 'latin1', echo= False)
     print('created engine')
 
     # drop tables if needed
-    # engine.execute(drop_tables_query)
-    # print('dropped pre-existing tables')
+    engine.execute(drop_tables_query)
+    print('dropped pre-existing tables')
 
     # creating tables
     engine.execute(create_tables_query)
@@ -76,20 +72,3 @@ if __name__ == '__main__':
     # input links data
     engine.execute(copy_table('links', PATH_LINKS))
     print('created links table')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
